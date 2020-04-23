d4b9aa172ee8b089682441e175c0e8375965b5ce
princeminz <samarpitminz@gmail.com>
2020-04-23 13:14:45 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,7 +8,6 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        f = open("requirements.txt", "r")
         message = cow.Cowacter().milk(os.system('ls'))
         self.wfile.write(message.encode())
         return
