1be7cfc6fa6f0f52364a458e3d52375ab9ec1a3d
princeminz <samarpitminz@gmail.com>
2020-04-23 13:23:06 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,6 +8,6 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        message = os.system('ls')
+        message = os.system('pwd')
         self.wfile.write(message.encode())
         return
